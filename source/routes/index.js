85a8c972a6bd976154ff8c242b3c4da60887f88a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-12-08 03:01:05 -0300

    add lottie-react-native e removendo telas inúteis

--- source/routes/index.js
+++ source/routes/index.js
@@ -7,15 +7,9 @@ import { createStackNavigator } from '@react-navigation/stack'
 import AppLoading from 'expo-app-loading'
 import { useFonts, Roboto_400Regular, Roboto_700Bold,  } from '@expo-google-fonts/roboto'
 
-import { Challenge } from '../screen/Challenge'
-import { Auth } from '../screen/Auth'
 import { Home } from '../screen/Home'
-import { PreChallenge } from '../screen/PreChallenge'
-import { Ranking } from '../screen/Ranking'
-import { Bonus } from '../screen/Bonus'
 import { routesPaths } from './routesPaths'
 import { SplashScreen } from '../screen/SplashScreen'
-import { Score } from '../screen/Score'
 
 
 LogBox.ignoreLogs(['Setting a timer for a long period'])
@@ -37,13 +31,7 @@ export function Routes() {
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name={routesPaths.splash} component={SplashScreen} />
-                <Stack.Screen name={routesPaths.auth} component={Auth} />
                 <Stack.Screen name={routesPaths.home} component={Home} />
-                <Stack.Screen name={routesPaths.preChallenge} component={PreChallenge} />
-                <Stack.Screen name={routesPaths.challenge} component={Challenge} />
-                <Stack.Screen name={routesPaths.score} component={Score} />
-                <Stack.Screen name={routesPaths.ranking} component={Ranking} />
-                <Stack.Screen name={routesPaths.bonus} component={Bonus} />
             </Stack.Navigator>
         </NavigationContainer>
     )
