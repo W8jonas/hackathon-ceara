0a5295aa4ef606475588cda89f4b1539f0691f81
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-12-10 02:14:41 -0300

    refazendo splash screen

--- source/screen/SplashScreen/index.js
+++ source/screen/SplashScreen/index.js
@@ -18,40 +18,19 @@ import { routesPaths } from '../../routes/routesPaths';
 export function SplashScreen({navigation}) {
     useEffect(() => {
         setTimeout(() => {
-            navigation.navigate(routesPaths.auth)
+            navigation.navigate(routesPaths.onboardingTransparency) 
         }, 1000);
     }, [])
 
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={baseGradients.linearDarkBlue.colors}
+            style={styles.container}
+            start={baseGradients.linearDarkBlue.start}
+            end={baseGradients.linearDarkBlue.end}
+        >
             <Image source={SplashImg} style={{ height: 142/1.1, width: 242/1.1 }} />
-
-            <Image source={Edukall} style={{ height: 3240/50, width: 5760/50, position: 'absolute', left: 10, bottom: 10 }} />
-
-            <View
-                style={{
-                    height: 130,
-                    width: 130,
-                    borderRadius: 130,
-                    position: 'absolute',
-                    top: -50,
-                    left: -50,
-                    backgroundColor: '#003466'
-                }} 
-            />
-
-            <View
-                style={{
-                    height: 130,
-                    width: 130,
-                    borderRadius: 130,
-                    position: 'absolute',
-                    bottom: -50,
-                    right: -50,
-                    backgroundColor: '#003466'
-                }} 
-            />
-        </View>
+        </LinearGradient>
     )
 }
 
@@ -60,6 +39,5 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#FFF'
     },
 })
