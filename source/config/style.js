be740cac6afb9f31b41c35a7c0570579e056eb18
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-12-10 02:13:11 -0300

    modificando paleta de cores

--- source/config/style.js
+++ source/config/style.js
@@ -2,13 +2,15 @@ export const baseColors = {
     blueDark: '#125DD6',
     blueMedium: '#125DD6',
     white: '#E5E5E5',
+    backgroundWhite: '#F2F3F5',
+    base2: '#006C30'
 }
 
 
 export const baseGradients = {
-    linearBlue: {
-        colors: ['#125DD6', '#33496C', '#7B90B2'],
-        start: { x: 1, y: 0 },
+    linearDarkBlue: {
+        colors: ['#091440', '#006C30'],
+        start: { x: 0, y: 0 },
         end: { x: 1, y: 1 },
     },
     linearBlue2: {
